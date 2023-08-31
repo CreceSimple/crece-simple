a003ac55938e65c4f2d7bee94a911d986881076c
Myntsu <dingodille16@hotmail.com>
2023-08-31 14:37:33 -0400

    Added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [sitemap()]
+  integrations: [sitemap(), partytown()]
 });
