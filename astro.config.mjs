e17789d99d1a127b8564bed0330f272e069c98f6
Myntsu <dingodille16@hotmail.com>
2023-10-20 12:24:42 -0300

    Adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   // ...
+  site: 'https://www.crecesimple.cl',
   integrations: [sitemap(), partytown()]
 });
